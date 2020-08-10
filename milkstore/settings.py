80b314a78ef3f849a5ee5d59cfa64f39d1f757b0
NabeelKM <nabeelkahlil403@gmail.com>
2020-08-10 19:33:14 +0700

    add product browser using boostrap grid cards as well as shopping list

--- milkstore/settings.py
+++ milkstore/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/css"),
-    os.path.join(BASE_DIR, "static/js"),
-    os.path.join(BASE_DIR, "static/product_imgs"),
+    os.path.join(BASE_DIR, "static"),
 ]
