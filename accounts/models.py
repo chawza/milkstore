209fba3bdb6f6be0f344a227a825dfda9730b30f
NabeelKM <nabeelkahlil403@gmail.com>
2020-08-11 10:09:26 +0700

    customer and store profile account has been made

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Account(models.Model):
-    username = models.CharField(max_length=32)
-    password = models.CharField(max_length=32)
-    date_create = models.DateField()
+    username = models.CharField(max_length=255)
+    password = models.CharField()
+    cardnumber = models.CharField(max_length=19)
+    date_create = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.username
+
+class Store(models.Model):
+    storename = models.CharField(max_length=255)
+    logo = models.CharField(max_length=255)
+    banner = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    date_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.storename
