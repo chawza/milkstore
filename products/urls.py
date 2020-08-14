1de66e725d3b7a422bf7fccb3945c7d99c97f183
NabeelKM <nabeelkahlil403@gmail.com>
2020-08-14 07:46:40 +0700

    adding view for retreiving image by pk

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:product_id>', views.product_info, name='product info'),
-    
+    path('img/<int:product_id>', views.get_image, name='get image'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, FileResponse
 from django.template import loader
+import os
 
 from .models import Product
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))).replace('\\', '/')
 
 # Create your views here.
 def index(request):
@@ -22,3 +24,9 @@ def product_info(request, product_id):
         'product' : product,
         'thumbnail' : 'products/'+product.thumbnail,
     })
+
+def get_image(request, product_id):
+    url = '/staticfiles/img/products/' + Product.objects.get(pk=product_id).thumbnail
+    url = BASE_DIR + url
+    
+    return FileResponse(open(url, 'rb'))
